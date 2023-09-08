fa8a2d46fe582dc6af882cf917b0ebb5cc5d1d52
TimSimon66 <huynhvantin125ttt@gmail.com>
2023-09-08 20:32:12 +0700

    video 25, chuc nang an va hien

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,34 +1,43 @@
 import React from "react";
-
+import "./css.css";
 class DisplayInfo extends React.Component {
+
+  state = {
+    changeListUser : true
+  }
+
+
+  handClickChange = () => {
+    this.setState ({
+      changeListUser : !this.state.changeListUser
+    })
+  }
+
+
   render() {
     const { user } = this.props;
     // const user = this.props.user;
     console.log(user);
     return (
       <div>
+
+         <div> Click here to {this.state.changeListUser ? "hide" : "display"} list user <button onClick={this.handClickChange}>Click</button></div>
+
+      {this.state.changeListUser && <div>
         {user.map((item, index) => {
-            console.log(index)
+          console.log(index);
+
           return (
-            <div key = {item.id}> 
-                <div>My name's {item.name}</div>
-                <div>My age {item.age}</div>
-                <hr />
+    
+            <div key={item.id} className= {+item.age > 18 ? "green" : "red"}>
+              <div>My name's {item.name}</div>
+              <div>My age {item.age}</div>
+              <hr />
             </div>
+            
           );
         })}
-
-        {/* <div>My name's {age}</div>
-             <div>My age {name}</div>
-             <hr />
-
-             <div>My name's {age}</div>
-             <div>My age {name}</div>
-             <hr />
-
-             <div>My name's {age}</div>
-             <div>My age {name}</div>
-             */}
+      </div>}
       </div>
     );
   }
--- src/components/MyComponenet.js
+++ src/components/MyComponenet.js
@@ -6,9 +6,9 @@ class MyComponent extends React.Component {
   //JSX
   state = {
     listUser : [
-      {id : 1, name : "TimSimon66", age : "20"},
+      {id : 1, name : "TimSimon66", age : "16"},
       {id : 2, name : "Eniqueen", age : "30"},
-      {id : 3, name : "VanTin", age : "25"},
+      {id : 3, name : "VanTin", age : "69"},
 
     ]
   }
