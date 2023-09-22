bd29a1159f2fa2a624a933705290bebb2b54fcf3
TimSimon66 <huynhvantin125ttt@gmail.com>
2023-09-22 19:16:03 +0700

    video 34

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,52 +1,48 @@
 import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg"
-class DisplayInfo extends React.Component {
+// class DisplayInfo extends React.Component {
 
-  constructor(props) {
-    console.log(">>> call me constructor: 1")
-    super(props);
-    this.state = {
-      changeListUser : true
-    }
-  }
+//   render() {
+//     console.log('>> Call me render')
+//     const { user } = this.props;
+//     // const user = this.props.user;
+//     // console.log(user);
+//     return (
 
+//       <div className="display-infor-container">
 
+//       {true &&
+//        <>
+//         {user.map((item, index) => {
+//           {/* console.log(user); */}
 
+//           return (
+    
+//             <div key={item.id} className= {+item.age > 18 ? "green" : "red"}>
+//               <div>My name's {item.name}</div>
+//               <div>My age {item.age}</div>
 
-  handClickChange = () => {
-    this.setState ({
-      changeListUser : !this.state.changeListUser
-    })
-  }
+//               <button onClick={() => this.props.handleDeleteUser(item.id)}>Delete</button>
+//               <hr />
+//             </div>
+            
+//           );
+//         })}
+//       </>}
+//       </div>
+//     );
+//   }
+// }
 
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('>> Call me didUpdate', this.props, prevProps)
-    if(this.props.user !== prevProps.user) {
-      if(this.props.user.length === 5){
-        alert("You got 5 user")
-      }
-  }
-}
-  componentDidMount(){
-    console.log('>> Call me didMont')
-    setTimeout(() => {
-      document.title = "Tim Hudson"
-    }, 3000)
-  }
-
-  render() {
-    console.log('>> Call me render')
-    const { user } = this.props;
-    // const user = this.props.user;
-    // console.log(user);
+const DisplayInfo = (props) => {
+  const { user } = props;
+  
     return (
-      <div className="display-infor-container">
-        {/* <img src = {logo} className="logo"/> */}
-         <div> Click here to {this.state.changeListUser ? "hide" : "display"} list user <button onClick={this.handClickChange}>Click</button></div>
 
-      {this.state.changeListUser && <>
+      <div className="display-infor-container">
+      {true &&
+       <>
         {user.map((item, index) => {
           {/* console.log(user); */}
 
@@ -56,7 +52,7 @@ class DisplayInfo extends React.Component {
               <div>My name's {item.name}</div>
               <div>My age {item.age}</div>
 
-              <button onClick={() => this.props.handleDeleteUser(item.id)}>Delete</button>
+              <button onClick={() => props.handleDeleteUser(item.id)}>Delete</button>
               <hr />
             </div>
             
@@ -65,7 +61,7 @@ class DisplayInfo extends React.Component {
       </>}
       </div>
     );
-  }
+  
 }
 
 export default DisplayInfo;
