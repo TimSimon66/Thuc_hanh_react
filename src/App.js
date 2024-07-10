a1dd6d591e2302ba49b62cde08e0320981a93d91
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-07-10 10:32:49 +0700

    change all

--- src/App.js
+++ src/App.js
@@ -7,72 +7,19 @@ import React from "react";
 import AddUserInfo from "./components/AddUserInfo";
 import DisplayInfo from "./components/DisplayInfo";
 
-// class App extends React.Component {
 
-//   state = {
-//     listUser : [
-//       {id : 1, name : "TimSimon66", age : "20"},
-//       {id : 2, name : "Eniqueen", age : "30"},
-//       {id : 3, name : "VanTin", age : "25"},
-
-//     ]
-//   }
-
-  
-//   render() {
-
-
-//     return (
-
-//       <div className="app-container">
-//         <MyComponent></MyComponent>
-//       </div>
-
-//     );
-//   }
-// }
-
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-
-//         <p>Hello world!</p>
-
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
 
 const App = (props) => {
 
 
 
-  // state = {
-  //   listUser : [
-  //     {id : 1, name : "TimSimon66", age : "20"},
-  //     {id : 2, name : "Eniqueen", age : "30"},
-  //     {id : 3, name : "VanTin", age : "25"},
 
-  //   ]
-  // }
+  return (
 
-    return (
+    <div>Hello world</div>
 
-      <div className="app-container">
-        <MyComponent></MyComponent>
-      </div>
+  );
 
-    );
-  
 }
 
 export default App;
--- src/components/MyComponenet.js
+++ src/components/MyComponenet.js
@@ -1,93 +1,14 @@
 import React, { useState } from "react";
-import AddUserInfo from "./AddUserInfo";
-import DisplayInfo from "./DisplayInfo";
-
-// class MyComponent extends React.Component {
-//   //JSX
-//   state = {
-//     listUser : [
-//       {id : 1, name : "TimSimon66", age : "16"},
-//       {id : 2, name : "Eniqueen", age : "30"},
-//       {id : 3, name : "VanTin", age : "69"},
-
-//     ]
-//   }
-
-//   handleNewUser = (userObject) => {
-//     // console.log(userObject);
-//     this.setState({
-//       listUser: [userObject, ...this.state.listUser],
-//     })
-//   }
-
-//   handleDeleteUser = (userId) => {
-//     let listUserClone = this.state.listUser
-//     listUserClone = listUserClone.filter(item => item.id !== userId)
-//     this.setState({
-//       listUser : listUserClone
-//     })
-//   }
-//   // DRY : don't repeat yourself
-//   render() {
-//     return (
-
-//       <>
-
-//       <div className="a">
-//       <AddUserInfo handleNewUser={this.handleNewUser}></AddUserInfo>
-//       <br />
-//       <br />
-//       <DisplayInfo
-//       user = {this.state.listUser}
-//       handleDeleteUser = {this.handleDeleteUser}
-//       ></DisplayInfo>
-//       </div>
-
-//       <div className="b">
-
-//       </div>
-
-//       </>
-//     );
-//   }
-// }
 
 const MyComponent = (props) => {
   //JSX
 
-  const [listUser, setListUser] = useState(
-    [
-      { id: 1, name: "TimSimon66", age: "16" },
-      { id: 2, name: "Eniqueen", age: "30" },
-      { id: 3, name: "VanTin", age: "69" },
-    ],
-  );
 
-  const handleNewUser = (userObject) => {
-    // console.log(userObject);
-    setListUser([userObject, ...listUser]);
-  };
 
-  const handleDeleteUser = (userId) => {
-    let listUserClone = listUser;
-    listUserClone = listUserClone.filter((item) => item.id !== userId);
-    setListUser(listUserClone)
-  };
 
-  // DRY : don't repeat yourself
   return (
     <>
-      <div className="a">
-        <AddUserInfo handleNewUser={handleNewUser}></AddUserInfo>
-        <br />
-        <br />
-        <DisplayInfo
-          user={listUser}
-          handleDeleteUser={handleDeleteUser}
-        ></DisplayInfo>
-      </div>
 
-      <div className="b"></div>
     </>
   );
 };
